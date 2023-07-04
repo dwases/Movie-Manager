5acc447a817db73e022a31cbff5492d089344f2d
dwases <dwases@gmail.com>
2023-07-04 17:07:54 +0200

    Add project files.

---
+++ MovieManager.Domain/Entity/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MovieManager.Domain.Common;
+
+namespace MovieManager.Domain.Entity
+{
+    public class Movie : BaseEntity
+    {
+        //public string Name { get; set; }
+        //^implemented by parent class
+        public string Synopsis { get; set; }
+    }
+}
